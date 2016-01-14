10296af427b9af7488ce80a6e82f3a55aaee8edf
Antoine GIRARD <sapk@users.noreply.github.com>
2016-01-14 16:21:47 +0100

    Create repo_branches.go

---
+++ repo_branches.go
@@ -0,0 +1,16 @@
+package gogs
+
+type Branch struct {
+	name      string         `json:"name"`
+	commit    *PayloadCommit `json:"commit"`
+}
+
+func (c *Client) ListRepoBranches(user, repo string) ([]*Branch, error) {
+	branches := make([]*Branch, 0, 10)
+	return branches, c.getParsedResponse("GET", fmt.Sprintf("/repos/%s/%s/branches", user, repo), nil, nil, &branches)
+}
+
+func (c *Client) GetRepoBranch(user, repo, branch string) (*Branch, error) {
+	b := new(Branch)
+	return b, c.getParsedResponse("GET", fmt.Sprintf("/repos/%s/%s/branches/%s", user, repo, branch), nil, nil, &b)
+}
