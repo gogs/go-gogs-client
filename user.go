89fbc3a75b07bb86ca47809d53f57892cc8c5443
Kim "BKC" Carlbäcker <kim.carlbacker@gmail.com>
2016-10-07 19:32:44 +0200

    Fix #3653

--- user.go
+++ user.go
@@ -11,7 +11,7 @@ import (
 // User represents a API user.
 type User struct {
 	ID        int64  `json:"id"`
-	UserName  string `json:"username"`
+	UserName  string `json:"login"`
 	FullName  string `json:"full_name"`
 	Email     string `json:"email"`
 	AvatarUrl string `json:"avatar_url"`
